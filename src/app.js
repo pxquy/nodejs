b6ed21012a2a9cb9a61867f887bae18ff734f900
Quý <phungxuanquy24721@gmail.com>
2025-09-20 17:34:40 +0700

    bài 2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import router from "./routers/index";
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use("/api/posts", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/routers/index.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/greet", (req, res) => {
+  const name = "Phùng Xuân Quí";
+  res.send(`Xin chào ${name}`);
+});
+
+router.get("/sum", (req, res) => {
+  const a = parseInt(req.query.a, 10) || 0;
+  const b = parseInt(req.query.b, 10) || 0;
+  const c = a + b;
+  return res.send(`tổng của: ${a} + ${b} là ${c}`);
+});
+
+export default router;
